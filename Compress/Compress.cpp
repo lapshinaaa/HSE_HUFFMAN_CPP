4b6db385cde490c2f2e69efa896b67ab6433cc7d
Anastasia Lapshina <anapluslap@gmail.com>
2023-03-20 18:34:56 +0300

    add struct, function for building minHeap

--- Compress/Compress.cpp
+++ Compress/Compress.cpp
@@ -1,1 +1,32 @@
 #include "Compress.h"
+#define MAX_TREE_HT 256
+#include <bits/stdc++.h>   //  includes every standard library and STL include file
+
+struct minHeapNode  // a struct of a node of the minimum heap
+{
+    char character;
+    int frequency;
+
+    // pointers to the ancestors of the current node
+    minHeapNode *left;
+    minHeapNode *right;
+
+    // initializing a node in our minimum heap
+    minHeapNode(char symbol, int occurence)
+    {
+        right = NULL;
+        left = NULL;
+        this->character = symbol;
+        this->frequency = occurence;
+    }
+
+};
+
+struct comparingNodes
+{
+    // a function for comparing nodes to build a minimum heap. Works like an object, hence - operator()
+    bool operator() (minHeapNode* left_node, minHeapNode* right_node)
+    {
+        return (left_node->frequency > right_node->frequency); // comparing frequency of the nodes
+    }
+};
--- Compress/Compress.h
+++ Compress/Compress.h
@@ -1,5 +1,5 @@
-
 #ifndef HSE_HUFFMAN_CPP_COMPRESS_H
 #define HSE_HUFFMAN_CPP_COMPRESS_H
 
 #endif //HSE_HUFFMAN_CPP_COMPRESS_H
+using namespace std;
